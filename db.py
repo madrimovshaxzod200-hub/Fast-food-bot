6efa0580863e3b160cda7b4cf083f8304f6b0d2c
madrimovshaxzod200-hub <madrimovshaxzod200@gmail.com>
2026-02-07 14:49:08 +0500

    Enhance database functionality for products
    Added functions to add products and retrieve categories and products from the database.

--- db.py
+++ db.py
@@ -1,19 +1,13 @@
 import sqlite3
 
-conn = sqlite3.connect("bot.db")
+conn = sqlite3.connect("bot.db", check_same_thread=False)
 cursor = conn.cursor()
 
 def init_db():
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS users(
-        id INTEGER PRIMARY KEY,
-        telegram_id INTEGER
-    )
-    """)
 
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS products(
-        id INTEGER PRIMARY KEY,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
         price INTEGER,
         category TEXT
@@ -21,3 +15,21 @@ def init_db():
     """)
 
     conn.commit()
+
+def add_product(name, price, category):
+    cursor.execute(
+        "INSERT INTO products(name, price, category) VALUES(?,?,?)",
+        (name, price, category)
+    )
+    conn.commit()
+
+def get_categories():
+    cursor.execute("SELECT DISTINCT category FROM products")
+    return cursor.fetchall()
+
+def get_products(category):
+    cursor.execute(
+        "SELECT name, price FROM products WHERE category=?",
+        (category,)
+    )
+    return cursor.fetchall()
