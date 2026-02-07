dfb24dc17cca81608ee3a36415795eab2855ee7b
madrimovshaxzod200-hub <madrimovshaxzod200@gmail.com>
2026-02-07 15:01:52 +0500

    Refactor start menu and add menu keyboard

--- keyboards.py
+++ keyboards.py
@@ -1,7 +1,18 @@
 from telegram import ReplyKeyboardMarkup
 
-def start_menu():
-    return ReplyKeyboardMarkup([
-        ["🏠 Zal", "🛵 Dastavka "],
+main_menu = ReplyKeyboardMarkup(
+    [
+        ["🏠 Zal", "🛵 Delivery"],
         ["📦 Buyurtmalarim"]
-    ], resize_keyboard=True)
+    ],
+    resize_keyboard=True
+)
+
+menu_keyboard = ReplyKeyboardMarkup(
+    [
+        ["🍔 Yegulik", "🥤 Ichimlik"],
+        ["🛒 Savat"],
+        ["⬅ Orqaga"]
+    ],
+    resize_keyboard=True
+)
