80ebcbb832aca110068726a9f6f1310f9640e7ef
madrimovshaxzod200-hub <madrimovshaxzod200@gmail.com>
2026-02-07 14:51:08 +0500

    Implement admin panel for product management

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,11 +1,46 @@
-from telegram import Update
-from telegram.ext import ContextTypes
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import ContextTypes, ConversationHandler, MessageHandler, filters, CommandHandler
 from config import ADMIN_IDS
+from db import add_product
+from states import NAME, PRICE, CATEGORY
 
 async def admin_panel(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.effective_user.id not in ADMIN_IDS:
         return
-    
+
+    keyboard = [["➕ Mahsulot qo‘shish"]]
     await update.message.reply_text(
-        "Admin panel 👨‍💻"
+        "Admin panel",
+        reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     )
+
+async def add_product_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Mahsulot nomini kiriting:")
+    return NAME
+
+async def get_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data["name"] = update.message.text
+    await update.message.reply_text("Narxini kiriting:")
+    return PRICE
+
+async def get_price(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data["price"] = int(update.message.text)
+
+    keyboard = [["🍔 Yegulik", "🥤 Ichimlik"]]
+
+    await update.message.reply_text(
+        "Kategoriya tanlang",
+        reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    )
+    return CATEGORY
+
+async def get_category(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    category = update.message.text
+    add_product(
+        context.user_data["name"],
+        context.user_data["price"],
+        category
+    )
+
+    await update.message.reply_text("✅ Mahsulot qo‘shildi")
+    return ConversationHandler.END
