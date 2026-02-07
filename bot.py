3ed902013065d93738c8e680586265f957d81225
madrimovshaxzod200-hub <madrimovshaxzod200@gmail.com>
2026-02-07 15:59:36 +0500

    Refactor bot.py for improved organization and clarity

--- bot.py
+++ bot.py
@@ -1,35 +1,63 @@
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ConversationHandler
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    ConversationHandler,
+    filters
+)
+
 from config import TOKEN
-from handlers.admin import admin_panel 
+from handlers.admin import admin_panel, add_product_start, get_name, get_price
 from handlers.client import *
+from keyboards import main_menu
 from states import *
 import db
-from handlers.client import open_menu, show_products
-from keyboards import main_menu
-from handlers.admin import start, add_product_start, get_name
-
 
 app = Application.builder().token(TOKEN).build()
 
-# Admin conversation
+
+# ===== Conversation (Mahsulot qo‘shish) =====
 conv = ConversationHandler(
-    entry_points=[MessageHandler(filters.Text("➕ Mahsulot qo‘shish"), add_product_start)],
+    entry_points=[
+        MessageHandler(filters.Text("+ Mahsulot qo‘shish"), add_product_start)
+    ],
     states={
-        NAME: [MessageHandler(filters.TEXT, get_name)],
-        PRICE: [MessageHandler(filters.TEXT, get_price)],
-        CATEGORY: [MessageHandler(filters.TEXT, get_category)]
+        NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_name)],
+        PRICE: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_price)],
+        CATEGORY: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_category)],
     },
     fallbacks=[]
 )
 
+
+# ===== START =====
 app.add_handler(CommandHandler("start", start))
+
+
+# ===== ADMIN PANEL =====
 app.add_handler(CommandHandler("admin", admin_panel))
 
+
+# ===== Conversation handler =====
 app.add_handler(conv)
-app.add_handler(MessageHandler(filters.Text("📋 Menyu"), menu))
+
+
+# ===== CLIENT MENYU =====
+app.add_handler(MessageHandler(filters.Text("📋 Menu"), menu))
 app.add_handler(MessageHandler(filters.TEXT, category_products))
 
-app.add_handler(MessageHandler(filters.Text(["🏠 Zal","🛵 Delivery"]), open_menu))
-app.add_handler(MessageHandler(filters.Text(["🍔 Yegulik","🥤 Ichimlik"]), show_products))
 
+# ===== Zal / Delivery =====
+app.add_handler(
+    MessageHandler(filters.Text(["🏠 Zal", "🚚 Delivery"]), open_menu)
+)
+
+
+# ===== Mahsulot ko‘rsatish =====
+app.add_handler(
+    MessageHandler(filters.Text(["🍔 Yegulik", "🥤 Ichimlik"]), show_products)
+)
+
+
+# ===== RUN =====
 app.run_polling()
