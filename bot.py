192e0f900385da31cfbd32192829079e8be23934
madrimovshaxzod200-hub <madrimovshaxzod200@gmail.com>
2026-02-14 11:50:35 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher, types,
+from aiogram import Bot, Dispatcher, types,F
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.filters import CommandStart
 from aiogram.fsm.state import State, StatesGroup
